786e85a2432695144f0670d321e37c737bc2b74e
Iulian Clita <iulian.clita@gmail.com>
2017-08-22 21:27:19 +0300

    Added doc file in parser package

---
+++ parser/doc.go
@@ -0,0 +1,5 @@
+// Package parser parses the given file input
+// It displays a paginated version of the file and optionally
+// filters the data according to user preferences
+// This aspect makes it suitable for inspecting log files of any size
+package parser
--- parser/parser.go
+++ parser/parser.go
@@ -1,7 +1,3 @@
-// Package parser parses the given file input
-// It displays a paginated version of the file and optionally
-// filters the data according to user preferences
-// This aspect makes it suitable for inspecting log files of any size
 package parser
 
 import (
