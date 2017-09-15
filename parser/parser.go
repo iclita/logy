d1e3b7aa92710dfac02dc75d9e2a7f364a16ea79
Iulian Clita <iulian.clita@gmail.com>
2017-09-15 19:51:09 +0300

    Added some useful comments

--- parser/parser.go
+++ parser/parser.go
@@ -52,6 +52,9 @@ func init() {
 
 // New returns a new parser object
 func New(file, text, filter string, lines, page int, noColor, withRegex bool) *parser {
+	// If the file path starts with "~"
+	// it means the user is on a Unix based OS
+	// and "~" represents the home directory path
 	if strings.HasPrefix(file, "~") {
 		user, err := user.Current()
 		if err != nil {
