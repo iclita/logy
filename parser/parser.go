ebd5218755c1660a1824374491dc1ea8b69b54ee
Iulian Clita <iulian.clita@gmail.com>
2017-10-28 22:53:09 +0300

    Refined code

--- parser/parser.go
+++ parser/parser.go
@@ -17,25 +17,6 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
-// Initial json Regexp
-var jsonReg *regexp.Regexp
-
-// Current accepted text types
-// This information is useful to let the parser know
-// how to interpret the given file input
-var textTypes = []string{
-	"plain",
-	"json",
-}
-
-var (
-	// Color functions to help display meaningful input
-	success = color.New(color.FgHiGreen, color.Bold).SprintFunc()
-	fail    = color.New(color.FgHiWhite, color.BgRed, color.Bold).SprintFunc()
-	alert   = color.New(color.FgHiYellow, color.Bold).SprintFunc()
-	info    = color.New(color.FgHiMagenta, color.Bold).SprintFunc()
-)
-
 // Parser type definition
 type Parser struct {
 	path   string
@@ -54,10 +35,31 @@ type stats struct {
 	matches int
 }
 
-// Initialize json regexp on package initialization
-func init() {
-	jsonReg = regexp.MustCompile(`\[?{".*:.*}\]?`)
+const (
+	// Size for the line scanner buffer
+	scanBuf = 64 * 1024 * 1024
+	// This is the input format which asks the user for new input data
+	inputFmt = "File: %s | Page [%d/%d]\nEnter page number to navigate\nEnter file id and page number separated by a comma to navigate to another file\nPress Ctrl+C to quit:"
+)
+
+// Initial json Regexp
+var jsonReg = regexp.MustCompile(`\[?{".*:.*}\]?`)
+
+// Current accepted text types
+// This information is useful to let the parser know
+// how to interpret the given file input
+var textTypes = []string{
+	"plain",
+	"json",
 }
+
+// Color functions to help display meaningful input
+var (
+	success = color.New(color.FgHiGreen, color.Bold).SprintFunc()
+	fail    = color.New(color.FgHiWhite, color.BgRed, color.Bold).SprintFunc()
+	alert   = color.New(color.FgHiYellow, color.Bold).SprintFunc()
+	info    = color.New(color.FgHiMagenta, color.Bold).SprintFunc()
+)
 
 // New returns a new parser object
 func New(path, text, filter string, lines, page int, noColor, withRegex bool, ext string) *Parser {
@@ -146,12 +148,13 @@ func (p *Parser) Parse() {
 	numPaths := len(paths)
 	// This should never happen :)
 	if numPaths == 0 {
-		log.Fatal("No paths found!")
+		exitWithError("No valid paths were found!")
 	}
 	// Set current page to what the parser gave us
 	currentPage := p.page
 	// Count all the lines and provide all page offsets
 	// These offsets help us navigate to any page instantly
+	// We will do this concurrently :)
 	// This is where all the "magic" happens
 	for _, path := range paths {
 		go p.countLines(path, sc)
@@ -194,7 +197,7 @@ func (p *Parser) Parse() {
 		return
 	}
 	// Show a message telling the user at which page we are right now and prompt to navigate to whatever page
-	fmt.Print(alert(fmt.Sprintf("File: %s | Page [%d/%d]\nEnter page number to navigate\nEnter file id and page number separated by a comma to navigate to another file\nPress Ctrl+C to quit:", currentPath, currentPage, numPages)), " ")
+	fmt.Print(alert(fmt.Sprintf(inputFmt, currentPath, currentPage, numPages)), " ")
 	// Start a new input scanner
 	in := bufio.NewScanner(os.Stdin)
 	// Scan for incoming input
@@ -204,15 +207,15 @@ func (p *Parser) Parse() {
 		id, page, err := extractNavigation(in.Text())
 		if err != nil {
 			fmt.Printf("\n%s\n\n", fail("Error! A valid number is required"))
-			fmt.Print(alert(fmt.Sprintf("File: %s | Page [%d/%d]\nEnter page number to navigate\nEnter file id and page number separated by a comma to navigate to another file\nPress Ctrl+C to quit:", currentPath, currentPage, numPages)), " ")
+			fmt.Print(alert(fmt.Sprintf(inputFmt, currentPath, currentPage, numPages)), " ")
 		} else {
 			switch {
 			case page < 1 || page > numPages:
 				fmt.Printf("\n%s\n\n", fail(fmt.Sprintf("Error! Page number must be between 1 and %d", numPages)))
-				fmt.Print(alert(fmt.Sprintf("File: %s | Page [%d/%d]\nEnter page number to navigate\nEnter file id and page number separated by a comma to navigate to another file\nPress Ctrl+C to quit:", currentPath, currentPage, numPages)), " ")
+				fmt.Print(alert(fmt.Sprintf(inputFmt, currentPath, currentPage, numPages)), " ")
 			case id > numPaths:
 				fmt.Printf("\n%s\n\n", fail(fmt.Sprintf("Error! ID number must be between 1 and %d", numPaths)))
-				fmt.Print(alert(fmt.Sprintf("File: %s | Page [%d/%d]\nEnter page number to navigate\nEnter file id and page number separated by a comma to navigate to another file\nPress Ctrl+C to quit:", currentPath, currentPage, numPages)), " ")
+				fmt.Print(alert(fmt.Sprintf(inputFmt, currentPath, currentPage, numPages)), " ")
 			default:
 				// If id = 0 the user wants to use the current file
 				// and does not want to change it
@@ -236,7 +239,7 @@ func (p *Parser) Parse() {
 				// Get output for display
 				output := p.getFilePage(currentPath, currentOffsets[currentPage-1])
 				fmt.Printf("\n%s\n", output)
-				fmt.Print(alert(fmt.Sprintf("File: %s | Page [%d/%d]\nEnter page number to navigate\nEnter file id and page number separated by a comma to navigate to another file\nPress Ctrl+C to quit:", currentPath, currentPage, numPages)), " ")
+				fmt.Print(alert(fmt.Sprintf(inputFmt, currentPath, currentPage, numPages)), " ")
 			}
 		}
 	}
@@ -246,7 +249,7 @@ func (p *Parser) Parse() {
 	}
 }
 
-// Gets the output for a new page on the input file
+// getFilePage gets the output for a new page on the input file
 func (p *Parser) getFilePage(path string, offset int64) string {
 	// Open the file
 	f, err := os.Open(path)
@@ -264,7 +267,7 @@ func (p *Parser) getFilePage(path string, offset int64) string {
 	// Start a new scanner
 	s := bufio.NewScanner(f)
 	// Set a larger buffer just in case
-	s.Buffer(nil, 64*1024*1024)
+	s.Buffer(nil, scanBuf)
 	// Current line. We need to keep track
 	// of all current line number to determine
 	// when a page ends
@@ -294,6 +297,7 @@ func (p *Parser) getFilePage(path string, offset int64) string {
 	return output.String()
 }
 
+// countLines extracts all line offsets
 // This is where all the "magic" happens
 // Here we count all the file lines
 // and extract a slice with all page offsets
@@ -402,7 +406,7 @@ func (p *Parser) countLines(path string, ch chan stats) {
 	}
 }
 
-// This determines if line matches a given filter
+// lineHits determines if line matches a given filter
 func (p *Parser) lineHits(line []byte) int {
 	// If no filter was provided then we do not care about this
 	if p.filter == "" {
@@ -418,7 +422,7 @@ func (p *Parser) lineHits(line []byte) int {
 	return bytes.Count(line, []byte(p.filter))
 }
 
-// This computes the final output
+// getOutput computes the final output
 func (p *Parser) getOutput(text string) string {
 	// Format input as JSON if needed
 	if p.text == "json" {
@@ -450,7 +454,7 @@ func (p *Parser) getOutput(text string) string {
 	return strings.Replace(text, p.filter, success(p.filter), -1)
 }
 
-// Get all file paths for a given root
+// getPaths retrieves file paths for a given root
 func (p *Parser) getPaths() []string {
 	// Define the final paths
 	var paths []string
@@ -482,7 +486,7 @@ func (p *Parser) getPaths() []string {
 	return paths
 }
 
-// Extract navigation details (file id and page number)
+// extractNavigation fetches navigation details (file id and page number)
 func extractNavigation(s string) (int, int, error) {
 	nums := strings.Split(s, ",")
 	if len(nums) > 2 {
@@ -509,7 +513,7 @@ func extractNavigation(s string) (int, int, error) {
 	return id, page, nil
 }
 
-// Displays the current stats for all files
+// renderStats Displays the current stats for all files
 func renderStats(fs []stats, id int) {
 	// Set table options
 	table := tablewriter.NewWriter(os.Stdout)
